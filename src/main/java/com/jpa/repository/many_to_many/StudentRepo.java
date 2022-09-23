7abd175dd85fc0107ed85e6127a940af4493541c
S Hulsurkar <sumitghrce@gmail.com>
2022-09-23 17:08:23 +0530

    Author validation

--- src/main/java/com/jpa/repository/many_to_many/StudentRepo.java
+++ src/main/java/com/jpa/repository/many_to_many/StudentRepo.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface StudentRepo extends JpaRepository<Student, Long> {
 
-        List<Student> findByNameContaining(String name); // findBy -> pre fix, Name ->  'Containing' keyword which is equivalent to 'LIKE' expression.
+        List<Student> findByNameContaining(String name); // findBy -> pre fix, Name -> 'Containing' keyword which is equivalent to 'LIKE' expression.
 }
