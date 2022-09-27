c0e7c3cdfb66cc26efd695e62583762464c79f8a
S Hulsurkar <sumitghrce@gmail.com>
2022-09-27 15:48:25 +0530

    Youtube video link added

--- src/main/java/com/jpa/JPAApplication.java
+++ src/main/java/com/jpa/JPAApplication.java
@@ -3,6 +3,8 @@ package com.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// https://www.youtube.com/watch?v=jCYonZey5dY&t=1800s
 @SpringBootApplication
 public class JPAApplication {
 
